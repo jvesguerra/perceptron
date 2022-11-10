cf908d2f7ffab5aab04b98744399abbbf380f090
jvesguerra <jvesguerra@up.edu.ph>
2022-11-10 19:05:40 +0800

    fully functional with discussion data

--- a.py
+++ a.py
@@ -4,14 +4,35 @@ To do:
 1. Create a table of the input txt [X]
 2. Determine classification y [X]
 3. adjusting weights done [X]
+4. STEP 2 DONE
 '''
 
 #functions
 def determineY(a,t):
     y = 0
-    if a >= t:
+    if a > t:
         y = 1
     return y
+
+def adjust_weight(td_row,arr,a,b,y):
+    w_counter = 0
+    for j in range(0, (tdata_table_col-1)):
+        w[w_counter] = w[w_counter] + (r*tdata_table[td_row][j]*(tdata_table[td_row][2] - y))
+        if w_counter == 1:
+            w_counter += 1
+            w[w_counter] = w[w_counter] + (r*b*(tdata_table[td_row][2] - y))
+        w_counter += 1
+    return arr
+
+# if not_converge == 1, all weights are not the same yet
+
+def weight_converge(all_arr,current_arr):
+    not_converge = 0
+    for j in range(0,len(all_arr)):
+        for i in range(0,len(current_arr)):
+            if current_arr[i] != all_arr[0][i]:
+                not_converge = 1
+    return not_converge
 
 #FILE READING
 
@@ -46,9 +67,11 @@ input_array.clear()
 row_len = len(training_data)  #4
 column_len = len(training_data[0]) - 1 #5
 tdata_array = []
+not_converge = 1
 
 # make tdata_table dynamic
 tdata_table = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+all_w = []
 w = [0,0,0]
 
 # fill tdata_table
@@ -63,76 +86,36 @@ for row in range(0,row_len):
 tdata_table_row = len(tdata_table)
 tdata_table_col = len(tdata_table[0])
 
-# check tdata_table
-# for i in range(0,4):
-#     for j in range(0,3):
-#         print(tdata_table[i][j], end="")
-#     print()
-
-
-for td_row in range(0, tdata_table_row):
-    print("ROW - ", td_row+1)
-    w_index = 0
-    a = 0
-    # Step 2 a - compute perceptron value
-    for td_col in range(0, (tdata_table_col-1)):
-        a = a + (tdata_table[td_row][td_col]*w[w_index])
-        print("w", w[w_index])
-        w_index += 1
-        if(td_col == (tdata_table_col-2)):
-            print("w", w[w_index])
-            a = a + (b * w[w_index])
-    #print("a", a)
-    # Step 2 b - determine classification, y
+while(not_converge != 0):
+    for td_row in range(0, tdata_table_row):
+        w_index = 0
+        a = 0
+        # Step 2 a - compute perceptron value
+        for td_col in range(0, (tdata_table_col-1)):
+            a = a + (tdata_table[td_row][td_col]*w[w_index])
+            w_index += 1
+            if(td_col == (tdata_table_col-2)):
+                a = a + (b * w[w_index])
+        # Step 2 b - determine classification, y
         y = determineY(a,t)
-        #print("y",y)
+
+
 
         # Step 2 c - adjust weights
         if td_row != 0:                             # we would be solving weights after the first row
-            for i in range(0,tdata_table_row):
-                w_counter = 0
-
-                # check values
-                #print("ROW - ", i+1)
-                # print("Y = ", y)
-                # print("Z = ",tdata_table[i][2])
-                # print("R = ", r)
-                # print("A = ", a)
-                # print()
-
-                for j in range(0, (tdata_table_col-1)):
-                    w[w_counter] = w[w_counter] + (r*tdata_table[i][j]*(tdata_table[i][2] - y))
-
-                    # check values
-                    # print("X = ",tdata_table[i][j])
-                    # print("W = ",w[w_counter], w_counter) 
-                    # print()
-
-                    if w_counter == 1:
-                        w_counter += 1
-                        #print("old W = ",w[w_counter],w_counter) 
-                        w[w_counter] = w[w_counter] + (r*b*(tdata_table[i][2] - y))
-
-                        #check values
-                        # print("X = ",tdata_table[i][j])
-                        # print("W = ",w[w_counter], w_counter) 
-                        # print()
-
-                    w_counter += 1
-
-                    
-            
-                print("New weights")
-                print("w[0]",w[0])
-                print("w[1]",w[1])
-                print("w[2]",w[2])
-                print()
-
-            print()
-
-
+            w = adjust_weight(td_row,w,a,b,y)
+            temp_w = w.copy()
+            all_w.append(temp_w)
     
-#print("test")
+    #print(all_w)
+    not_converge = weight_converge(all_w,temp_w)
+    all_w.clear()
+    all_w.append(temp_w)
+    print("Not Converge = ", not_converge)
+        
+
+
+   
 
 
 
--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 arr1 = [0,0,0]
 
-print(arr1)
+arr2 = []
+
+arr2.append(arr1)
+
+print(arr2)
+
+arr2.append(arr1)
 
 
-def change_arr(arr):
-    for i in range(0, len(arr)):
-        arr[i] = 1
-    return arr
-
-arr1 = change_arr(arr1)
-
-print(arr1)
+print(arr2)
